8296816dd94e29f3f00410c94b3bed03e7d34d20
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-06-16 12:32:58 -0300

    deploy project do github for the first time

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const user = 'rocketinsta'
+const password = '2bsLVYVdR2NiiFxu'
+
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0-6sdqi.mongodb.net/test?retryWrites=true&w=majority`, {
+    useNewUrlParser: true
+})
+
+app.use((req, res, next) => {
+    req.io = io
+    next()
+})
+
+app.use(cors())
+app.use(require('./routes'))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
+server.listen(3001)
+
