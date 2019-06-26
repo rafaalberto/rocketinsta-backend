a063ca4eabcdb0154b11e686ddd490985a43b480
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-06-26 20:29:01 -0300

    adicionando procfile para github

--- src/index.js
+++ src/index.js
@@ -22,5 +22,5 @@ app.use((req, res, next) => {
 app.use(cors())
 app.use(require('./routes'))
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')))
-server.listen(3001)
+server.listen(process.env.PORT || 3000)
 
