bec93f8fa9b36bd6b8d3ab17652e6e253951b9b6
Rafael Alberto <rafael.alberto1703@gmail.com>
2019-07-01 10:22:09 -0300

    ajustando para subir arquivos no s3 aws

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,11 +1,23 @@
 const multer = require('multer')
 const path = require('path')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
 
-module.exports = {
-    storage: new multer.diskStorage({
+const storagesTypes = {
+    local: multer.diskStorage({
         destination: path.resolve(__dirname, '..', '..', 'uploads'),
         filename: function(req, file, callback) {
             callback(null, file.originalname)
         }
+    }),
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'rocketinsta-upload',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read'
     })
+}
+
+module.exports = {
+    storage: storagesTypes['s3'] 
 }
--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -10,20 +10,22 @@ module.exports = {
     },
     async store(req, res) {
         const { author, place, description, hashtags } = req.body
-        const { filename: image  } = req.file
-        
-        const [name] = image.split('.')
-        const fileName = `${name}.jpg`
+        const { filename: image } = req.file
+        let { location: fileName } = req.file
 
-        await sharp(req.file.path)
-            .resize(500)
-            .jpeg({ quality: 100 })
-            .toFile(
-                path.resolve(req.file.destination, 'resized', fileName)
-            )
-
-        fs.unlinkSync(req.file.path)
-
+        if (image !== undefined) {
+            const [name] = image.split('.')
+            fileName = `${name}.jpg`
+    
+            await sharp(req.file.path)
+                .resize(500)
+                .jpeg({ quality: 100 })
+                .toFile(
+                    path.resolve(req.file.destination, 'resized', fileName)
+                )
+    
+            fs.unlinkSync(req.file.path)
+        }
         const post = await Post.create({
             author,
             place,
